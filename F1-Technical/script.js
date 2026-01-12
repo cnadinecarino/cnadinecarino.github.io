3300ca9d1c9e07159f692ebb0c4194c505609c4f
cnadinecarino <nadinecarino09@gmail.com>
2026-01-12 23:40:26 +0800

    Delete F1-Technical directory

--- F1-Technical/script.js
+++
@@ -1,6 +0,0 @@
-function sendMessage(e) {
-    e.preventDefault();
-    let name = document.getElementById("name").value;
-    document.getElementById("response").innerText =
-        "Thank you, " + name + ". Your message has been sent to ESSENCE.";
-}

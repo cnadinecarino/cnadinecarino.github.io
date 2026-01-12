58d7c45470955ace360d47189edea6a71a10e634
cnadinecarino <nadinecarino09@gmail.com>
2026-01-12 23:13:23 +0800

    Delete Activity 2 directory

--- Activity 2/script.js
+++
@@ -1,6 +0,0 @@
-function sendMessage(e) {
-    e.preventDefault();
-    let name = document.getElementById("name").value;
-    document.getElementById("response").innerText = "Thank You, " + name + "! Your message has been sent.";
-
-}
